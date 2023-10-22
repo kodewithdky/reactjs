2ceaf9602b2d3d4e8f4885198f08a4abca9cbac3
kodewithdky <kodewithdky@gmail.com>
2023-10-22 15:31:49 -0500

    Introduction of hooks and useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    setCounter(counter+1)
+  };
+  const recreaseValue=()=>{
+    setCounter(counter-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>kodewithdky</h1>
+      <h3>Counter Value: {counter}</h3>
+
+      <br />
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={recreaseValue}>Decrease Value</button>
     </div>
   );
 }
