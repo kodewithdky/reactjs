1b13a26aa11396a9f2897acf2c2f89b7834a3c6f
kodewithdky <kodewithdky@gmail.com>
2023-10-22 17:29:56 -0500

    project change multiple background color

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import { useState } from "react";
 import "./App.css";
-import Card from "./components/Card";
 
 function App() {
-  const user={
-    name:'kodewithdky',
-    channel:'kodewithdky',
-    location:'Lucknow'
-  }
+  const [color,setColor]=useState("green")
   return (
-    <>
-     <div className="relative bg-green-500 p-2 inline-block rounded-md left-[43%] top-[5vh]">
-      <h1 className="text-2xl">Tailwind Test</h1>
+    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+       <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white rounded-3xl px-3 py-2">
+        <button onClick={()=>setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+        <button onClick={()=>setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+        <button onClick={()=>setColor("white")} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:"white"}}>white</button>
+        <button onClick={()=>setColor("black")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>Black</button>
+        <button onClick={()=>setColor("yellow")} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:"yellow"}}>Yellow</button>
+        <button onClick={()=>setColor("pink")} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:"pink"}}>Pink</button>
+       </div>
      </div>
-     <Card username="kodewithdky" btnText="click me"/>
-     <Card username="DK. YaDav" btnText="View"/>
-    </>
+    </div>
   );
 }
 
