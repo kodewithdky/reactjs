771d5fc5a590a93481f4234ad42e61fb520cb51a
kodewithdky <kodewithdky@gmail.com>
2023-10-24 19:54:00 -0500

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,93 +1,82 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [length, setLength] = useState(6);
-  const [numAllowed, setNumAllowed] = useState(false);
-  const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
-  //useRef hook
-  const passwordRef = useRef(null);
+  const currencyInfo = useCurrencyInfo(from);
 
-  const passwordGEnerator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (numAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$%^&*()+/";
+  const options = Object.keys(currencyInfo);
 
-    for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
-    }
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
 
-    setPassword(pass);
-  }, [length, numAllowed, charAllowed, setPassword]);
-
-  const copyPasswordToClipBoard = useCallback(() => {
-    passwordRef.current?.select()
-    window.navigator.clipboard.writeText(password);
-  }, [password]);
-
-  useEffect(() => {
-    passwordGEnerator();
-  }, [length, numAllowed, charAllowed, passwordGEnerator]);
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8  bg-gray-700 text-black">
-        <h1 className="text-white text-center">Password genrator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            className="outline-none w-full py-1 my-1 rounded-lg px-3"
-            type="text"
-            value={password}
-            ref={passwordRef}
-            readOnly
-          />
-          <button
-            onClick={copyPasswordToClipBoard}
-            className="bg-white mx-2 text-black outline-none py-1 my-1 rounded-lg px-3"
-          >
-            Copy
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-2 text-white">
-          <div className="flex items-center gap-x-1  mb-3">
-            <input
-              className="cursor-pointer my-1"
-              id="length"
-              type="range"
-              min={6}
-              max={60}
-              value={length}
-              onChange={(e) => setLength(e.target.value)}
-            />
-            <label htmlFor="length">Length: {length}</label>
-          </div>
-          <div className="flex items-center gap-x-1 mb-3">
-            <input
-              className="cursor-pointer my-1 "
-              id="numAllowed"
-              type="checkbox"
-              defaultChecked={numAllowed}
-              onChange={() => {
-                setNumAllowed((prev) => !prev);
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://cdn.pixabay.com/photo/2016/10/16/13/06/new-york-1745089_1280.jpg')`,
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
               }}
-            />
-            <label htmlFor="numAllowed">Numbers</label>
-          </div>
-          <div className="flex items-center gap-x-1  mb-3">
-            <input
-              className="cursor-pointer my-1"
-              id="numAllowed"
-              type="checkbox"
-              defaultChecked={charAllowed}
-              onChange={() => {
-                setCharAllowed((prev) => !prev);
-              }}
-            />
-            <label htmlFor="numAllowed">Characters</label>
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(amount) => setAmount(amount)}
+                  selectCurrency={from}
+                  onAmountChange={(amount)=>setAmount(amount)}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  Swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency)=>setTo(currency)}
+                  selectCurrency={from}
+                  amountDisable
+                />
+              </div>
+              <button
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+                type="submit"
+                onClick={convert}
+              >
+                Convert {from.toUpperCase} to {to.toUpperCase}
+              </button>
+            </form>
           </div>
         </div>
       </div>
---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+ async function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://v6.exchangerate-api.com/v6/a47e38acc63bc65592ca48f6/latest/${currency}`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
