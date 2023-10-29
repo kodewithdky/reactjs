7c66f67cbfa9e80720f13eb1a54d88f73f35262c
kodewithdky <kodewithdky@gmail.com>
2023-10-28 21:28:27 -0500

    context api part-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
+import ThemBtn from "./components/ThemBtn";
+import Card from "./components/Card";
+import { ThemeProvider } from "./context/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <>
-      <Login />
-      <Profile />
-    </>
+    <ThemeProvider value={(themeMode, darkTheme, lightTheme)}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import UserContextProvider from "./context/UserContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <UserContextProvider>
+
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </UserContextProvider>
 );
